529c0becd721117785b147a8cf6a4642aca2bdfc
Adam_Young <96896512+conchaking@users.noreply.github.com>
2023-05-14 10:31:06 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+console.log("Linked");
+var student1 = "Adam";//declaration and assign
+var student2 = "Marquice";
+var student3 = "DeJuan";
+var num1 = 10;
+var num2 = 5;
+
+//Netflix Categories//
+var cat1 = "Film Noir";
+var cat2 = "New Releases";
+var cat3 = "Comedy"; 
+var cat4 = "Romantic"; 
+var cat5 = "Action"; 
+var cat6 = "Drama";
+var cat7 = "Recent";
+var cat8 = "TV Shows";
+var cat9 = "Mylist";
+var cat10 = "Fantasy"; 
+var cat11 = "SciFi";
+var cat12 = "Award Winning";
+var cat13 = "Vintage Crime";
+var cat14 = "Suspense"; 
+var cat15 = "Critically Acclaimed"; 
+var cat16 = "Family Movie";
+var cat17 = "Mid Century"; 
+var cat18 = "Blockbuster Movies";
+var cat19 = "Popular";
+var cat20 = "Star Studded"; 
+
+student1 = 100;
+
+document.write(`<p> ${student1} </p>`);
+document.write(`<p> ${student2} </p>`);
+document.write(`<p> ${student3} </p>`);
+var sum = num1 + num2;
+document.write(`<p> ${sum} </p>`);
+
+document.write(`<p> ${cat1} </p>`);
+document.write(`<p> ${cat2} </p>`);
+document.write(`<p> ${cat3} </p>`);
+document.write(`<p> ${cat4} </p>`);
+document.write(`<p> ${cat5} </p>`);
+document.write(`<p> ${cat6} </p>`);
+document.write(`<p> ${cat7} </p>`);
+document.write(`<p> ${cat8} </p>`);
+document.write(`<p> ${cat9} </p>`);
+document.write(`<p> ${cat10} </p>`);
+document.write(`<p> ${cat11} </p>`);
+document.write(`<p> ${cat12} </p>`);
+document.write(`<p> ${cat13} </p>`);
+document.write(`<p> ${cat14} </p>`);
+document.write(`<p> ${cat15} </p>`);
+document.write(`<p> ${cat16} </p>`);
+document.write(`<p> ${cat17} </p>`);
+document.write(`<p> ${cat18} </p>`);
+document.write(`<p> ${cat19} </p>`);
+document.write(`<p> ${cat20} </p>`);
